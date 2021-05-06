b68ac0c92934984c7fd3d6f16054196c082c805d
iapicca <iapicca@gmail.com>
2021-05-06 12:39:10 +0300

    change theme colors

--- lib/ui/all.dart
+++ lib/ui/all.dart
@@ -3,5 +3,6 @@ export 'edit.dart';
 export 'fab.dart';
 export 'fragment/all.dart';
 export 'home.dart';
+export 'material.dart';
 export 'new.dart';
 export 'todo/all.dart';
--- lib/ui/fragment/background_info.dart
+++ lib/ui/fragment/background_info.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-///
+/// text shown when no items
 const noItemYet = Center(
   child: Text(
     'no item yet',
